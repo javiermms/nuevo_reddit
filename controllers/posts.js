b61fdf79658ee28f299921005a8fe43864d503ea
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-05-29 13:53:01 -0700

    Users can submit and save posts

---
+++ controllers/posts.js
@@ -0,0 +1,20 @@
+const Post = require('../models/post');
+
+module.exports = app => {
+
+    //NEW POST FORM
+    app.get('/posts/new', (req, res) => {
+        res.render('posts-new', {});
+    });
+
+    // CREATE
+    app.post('/posts/new', (req, res) => {
+        // INSTANTIATE INSTANCE OF POST MODEL
+        const post = new Post(req.body);
+        // SAVE INSTANCE OF POST MODEL TO DB
+        post.save((err, post) => {
+        // REDIRECT TO THE ROOT
+        return res.redirect(`/`);
+        })
+    });
+};
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 var exphbs = require('express-handlebars');
 const express = require('express')
+const Post = require('./models/post');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const app = express()
 const port = 3000
 
+// Handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Use Express Validator
+app.use(expressValidator());
 
 //INDEX
 app.get('/', (req, res) => {
     res.render('index', {});
 });
 
+
+require('./controllers/posts.js')(app);
+require('./data/nuevo-reddit-db');
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
