c515408a13de408a4e0bb2b4c397d45ec6f01aab
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-05-30 13:16:06 -0700

    Users can see Subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -42,4 +42,15 @@ module.exports = app => {
           });
       });
 
+    // SUBREDDIT
+    app.get("/n/:subreddit", function(req, res) {
+        Post.find({ subreddit: req.params.subreddit })
+        .then(posts => {
+            res.render("posts-index", { posts });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    });
+
 };
